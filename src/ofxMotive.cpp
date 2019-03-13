4fc3da7ed1e20ed6056541b60397769209f1b297
Ben Snell <bensnell@localprojects.com>
2019-03-13 10:34:19 -0400

    Update

--- src/ofxMotive.cpp
+++ src/ofxMotive.cpp
@@ -6,7 +6,25 @@ ofxMotive::ofxMotive() {
 }
 
 // --------------------------------------------------------------
+ofxMotive::~ofxMotive() {
+	stop();
+	waitForThread(false, 3000);
+}
+
+// --------------------------------------------------------------
 void ofxMotive::setupParams() {
+
+	RUI_NEW_GROUP("ofxMotive");
+	RUI_SHARE_PARAM_WCN("Force Try Connect", bForceTryConnect);
+	RUI_SHARE_PARAM_WCN("Try Connect Timestep", tryConnectTimestep, 0, 10000);
+	RUI_SHARE_PARAM_WCN("Max Try Connect Attempts", maxTryConnectAttempts, 1, 10);
+	RUI_SHARE_PARAM_WCN("Profile Path", profilePath);
+	RUI_SHARE_PARAM_WCN("Reload Profile", bForceLoadProfile);
+	RUI_SHARE_PARAM_WCN("Force Disconnect", bForceDisconnect);
+	RUI_SHARE_PARAM_WCN("Process All Frames", bProcessAllFrames);
+
+	// flush the queues
+
 
 	
 
@@ -16,26 +34,219 @@ void ofxMotive::setupParams() {
 // --------------------------------------------------------------
 void ofxMotive::start() {
 
-	TT_Initialize();
+	// Begin the thread that will connect.
+	// If blocking is true, then any lock on this thread will wait until its acquired. Otherwise, 
+	// attempting to lock it will either return true or false.
+	startThread(bBlocking);
+	connect();
+}
 
+// --------------------------------------------------------------
+MotiveState ofxMotive::getState() {
+	return state;
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::isState(MotiveState _state) {
+	return state == _state;
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::isConnected() {
+	return isState(MOTIVE_CONNECTED);
+}
+
+// --------------------------------------------------------------
+void ofxMotive::connect() {
+	// Change the state of this thread
+	if (isConnected()) {
+		ofLogVerbose("ofxMotive") << "ofxMotive is already connected.";
+	}
+	else if (isState(MOTIVE_TRY_CONNECT)) {
+		ofLogVerbose("ofxMotive") << "ofxMotive is already trying to connect";
+	}
+	else {
+		startTryConnect();
+	}
+}
+
+// --------------------------------------------------------------
+void ofxMotive::startTryConnect() {
+	setState(MOTIVE_TRY_CONNECT);
+	lastTryConnectTime = -tryConnectTimestep;
+	tryConnectCounter = 0;
 }
 
 // --------------------------------------------------------------
 void ofxMotive::stop() {
+	// Disconnect the system and cameras
+	disconnect();
+
+	// Stop the main thread
+	stopThread();
+}
+
+// --------------------------------------------------------------
+void ofxMotive::disconnect() {
+	// If we're connected, disconnect all cameras first
+	if (!isState(MOTIVE_DISCONNECTED)) {
+		setState(MOTIVE_DISCONNECTED);
+		shutdown();
+	}
+}
+
+// --------------------------------------------------------------
+void ofxMotive::setState(MotiveState _state) {
+	state = _state;
+};
+
+// --------------------------------------------------------------
+bool ofxMotive::isSuccess(NPRESULT result, string fnctName) {
+	if (result == NPRESULT_SUCCESS) return true;
+	string resultString(TT_GetResultString(result));
+	ofLogError("ofxMotive" + (fnctName.empty() ? "" : (" : " + fnctName))) << resultString;
+	return false;
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::initialize() {
+	return isSuccess(TT_Initialize(), "Initialize");
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::shutdown() {
+	// this automatically saves the system calibration file "System Calibration.cal"
+	return isSuccess(TT_Shutdown(), "Shutdown");
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::loadProfile() {
+	if (profilePath == "") return false;
+	return isSuccess(TT_LoadProfile(profilePath.c_str()), "Load Profile");
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::loadCalibration() {
+	if (calibrationPath == "") return false;
+	return isSuccess(TT_LoadCalibration(calibrationPath.c_str()), "Load Calibration");
+}
+
+// --------------------------------------------------------------
+void ofxMotive::threadedFunction() {
+
+	while (isThreadRunning()) {
+
+		// Update parameters if any have changed
+		if (bForceTryConnect) {
+			bForceTryConnect = false;
+			RUI_PUSH_TO_CLIENT();
+			startTryConnect();
+		}
+		if (bForceLoadProfile) {
+			bForceLoadProfile = false;
+			RUI_PUSH_TO_CLIENT();
+			loadProfile();
+		}
+		if (bForceDisconnect) {
+			bForceDisconnect = false;
+			RUI_PUSH_TO_CLIENT();
+			disconnect();
+		}
+
+		// Do something different depending on the state
+		switch (state) {
+		case MOTIVE_TRY_CONNECT: {
+
+			// Try to connect every timestep
+			unsigned long thisTime = ofGetElapsedTimeMillis();
+			if (thisTime - lastTryConnectTime >= tryConnectTimestep) {
+				lastTryConnectTime = thisTime;
+				tryConnectCounter++;
+
+				// Try to initialize the system
+				if (initialize()) {
+					setState(MOTIVE_CONNECTED);
+					loadProfile(); // load profile
+					loadCalibration();
+					update(); // init camera list
+				}
+			}
+
+			// If we've surpassed the limit, then stop trying to connect
+			if (tryConnectCounter >= maxTryConnectAttempts) {
+				ofLogNotice("ofxMotive") << "Exceeded max number of connection attempts. Exiting.";
+				setState(MOTIVE_DISCONNECTED);
+			}
+
+		}; break;
+		case MOTIVE_CONNECTED: {
+
+			// Get all new information from the cameras
+			processNewData();
+
+		}; break;
+		case MOTIVE_DISCONNECTED: default: {
+
+			// Don't run this thread too much if disconnected
+			ofSleepMillis(200);
+
+		}; break;
+		}
+	}
+}
+
+// --------------------------------------------------------------
+bool ofxMotive::update(bool bSingleFrame) {
+	if (bSingleFrame) {
+		return TT_UpdateSingleFrame() == NPRESULT_SUCCESS;
+	}
+	else {
+		return TT_Update() == NPRESULT_SUCCESS;
+	}
+}
+
+// --------------------------------------------------------------
+void ofxMotive::processNewData() {
+
+	// While we can update the camera feed, update the information we're receiving
+	while (update(bProcessAllFrames)) {
+
+		// Update the camera information (fps, serial numbers, etc.)
 
 
+
+		// Get the 3D information
+		// TT_FrameMarkerCount()
+		// x,y,z,residual,label
+		// which cameras are contributing?
+
+
+		// lock
+		// store the 3D info in a queue
+		// unlock
+
+
+	}
+}
+
+// --------------------------------------------------------------
+void ofxMotive::updateCameraInfo() {
+	
+	// Get the total number of cameras
+	//int nCameras = 
+	//for (int index = 0; index < )
+
+
+	// If we have a different number of cameras, update the list we're storing
+
+	// Once a second, match the serial numbers
+
+
+	// Iterate through all cameras
 
 
 
 }
-
-// --------------------------------------------------------------
-
-// --------------------------------------------------------------
-
-// --------------------------------------------------------------
-
-// --------------------------------------------------------------
 
 // --------------------------------------------------------------
 
--- src/ofxMotive.h
+++ src/ofxMotive.h
@@ -2,16 +2,17 @@
 
 #include "ofMain.h"
 
+// If we're simulating motive, don't load 
+//#ifndef SIMULATE_MOTIVE
 #include <windows.h>
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include "NPTrackingTools.h"
+//#endif
 
 #include "ofxRemoteUIServer.h"
-
 
 enum MotiveState {
 	MOTIVE_DISCONNECTED = 0,
@@ -22,42 +23,87 @@ enum MotiveState {
 class ofxMotive : private ofThread {
 public:
 
-	///// \brief Create an object to connect with motive's cameras. There should only be one per program.
+	/// \brief Create an object to connect with motive's cameras. There should only be one per program.
 	ofxMotive();
+	~ofxMotive();
 
-	///// \brief Setup the RUI Params associated with this addon
+	/// \brief Setup the RUI Params associated with this addon
 	void setupParams();
 
-	///// \brief Begin streaming and reconstructing information from the cameras
+	/// \brief Begin streaming and reconstructing information from the cameras
 	void start();
 
-	///// \brief Check if we're connected to the cameras and streaming info
-	//bool isConnected();
+	/// \brief Check if we're connected to the cameras and streaming info
+	bool isConnected();
 
-	///// \brief Stop streaming and reconstructing
+	bool isState(MotiveState _state);
+
+	/// \brief Try to connect after some time of being disconnected
+	void connect();
+
+	/// \brief Disconnect the system and disable all cameras.
+	void disconnect();
+
+	/// \brief Stop streaming and reconstructing
 	void stop();
 
-	///// \brief Get the state of this addon
-	//MotiveState getState();
+	/// \brief Get the state of this addon
+	MotiveState getState();
 
-	///// \brief Get the number of connected cameras
-	//void getNumCameras();
-	//
-	///// \brief Get a pointer to all camera objects
-	////CameraList* getCameras();
+	/// \brief Get the number of connected cameras
+	void getNumCameras();
+	
+	/// \brief Get a pointer to all camera objects
+	//CameraList* getCameras();
 
-	///// \brief Get the observed 2D points from a specific camera feed
-	//vector<glm::vec2> get2DPoints(int camID);
+	/// \brief Get the observed 2D points from a specific camera feed
+	vector<glm::vec2> get2DPoints(int camID);
 
-	///// \brief Get the reconstructed 3D points
-	//vector<glm::vec3> get3DPoints();
+	/// \brief Get the reconstructed 3D points
+	vector<glm::vec3> get3DPoints();
+
+	/// \brief Draw any debug info
+	void drawDebugInfo();
 
 
 private:
 
+	bool isSuccess(NPRESULT result, string fnctName = "");
+	//void logResult(NPRESULT result, string functionName = "");
+
+	MotiveState state = MOTIVE_DISCONNECTED;
+	void setState(MotiveState _state);
+
+	void threadedFunction();
+	bool bBlocking = true;
+
 	//CameraList cams;
 
+	void startTryConnect();
+	long lastTryConnectTime = 0;
+	int tryConnectTimestep = 3000; // every 3 seconds
+	int tryConnectCounter = 0;
+	int maxTryConnectAttempts = 5;
+	bool bForceTryConnect = false;
 
+	bool bForceDisconnect = false;
+
+	string profilePath = ""; // should be a .motive file (actually xml)
+	bool bForceLoadProfile = false;
+	bool loadProfile();
+
+	string calibrationPath = ""; // should be a .cal file
+	bool bForceLoadCalibration = false;
+	bool loadCalibration();
+
+	bool initialize();
+	bool update(bool bSingleFrame = false);
+	bool shutdown();
+
+	void processNewData();
+	bool bProcessAllFrames = true;
+
+	void updateCameraInfo();
 
 
 };
